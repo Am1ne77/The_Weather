dccf6c5ace884d12b30489a52d98edd4868eb401
Amine <am1ne.maalouf7@gmail.com>
2022-08-22 18:30:20 +0300

    First time users now have to put an openweather key

--- Connected.js
+++ Connected.js
@@ -1,12 +1,26 @@
-
-function myFunction() {
-    const connectScreen = document.getElementById("connectScreen");
-    const allData = document.getElementById("allData");
-    if (localStorage.getItem("key") !== null) {
-      connectScreen.style.display = "none";
-      allData.style.display = "grid"
-    } 
+function Toogle() {
+  const connectScreen = document.getElementById("connectScreen");
+  const allData = document.getElementById("allData");
+  connectScreen.style.display = "none";
+  allData.style.display = "grid";
 }
-//localStorage.setItem("key", "034fa528ccf9f7ebe825bbbff6f5e98f");
+
+document.querySelector(".key-bar").addEventListener("keyup", function (event) {
+  const currkey = document.querySelector(".key-bar").value;
+  if (event.key == "Enter") {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=paris&units=metric&appid=" +
+        currkey
+    ).then((response) => {
+      if (!response.ok) {
+        alert("Your key is not valid");
+        throw new Error("Your key is not valid");
+      }
+      localStorage.setItem("key", currkey);
+      Toogle();
+      getWeather("Paris");
+    });
+  }
+});
+
 //localStorage.removeItem("key");
-myFunction();
--- script.js
+++ script.js
@@ -1,58 +1,70 @@
-const key = '034fa528ccf9f7ebe825bbbff6f5e98f';
-
 const getWeather = (city) => {
-    fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city +"&units=metric&appid=" + key
-      ).then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => display(data));
-}
+  const key = localStorage.getItem("key");
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=metric&appid=" +
+      key
+  )
+    .then((response) => {
+      if (!response.ok) {
+        alert("No weather found.");
+        throw new Error("No weather found.");
+      }
+      return response.json();
+    })
+    .then((data) => display(data));
+};
 
 const display = (data) => {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".cityname").innerText = 'Weather in ' + name;
-    document.querySelector(".temperature").innerText = temp + '°C';
-    document.querySelector(".humidity").innerText = "Humidity: " + humidity + '%';
-    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h"
-    document.querySelector(".description").innerText = titleCase(description);
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+  document.querySelector(".cityname").innerText = "Weather in " + name;
+  document.querySelector(".temperature").innerText = temp + "°C";
+  document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+  document.querySelector(".description").innerText = titleCase(description);
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
 
-    document.querySelector(".weatherdata").classList.remove("loading");
+  document.querySelector(".weatherdata").classList.remove("loading");
 
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-}
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1600x900/?" + name + "')";
+};
 
 const search = () => {
-    getWeather(document.querySelector(".search-bar").value);
-}
+  getWeather(document.querySelector(".search-bar").value);
+};
 
 document.querySelector(".search-button").addEventListener("click", function () {
-    search();
-  });
+  search();
+});
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
       search();
     }
   });
 
-getWeather("Paris");
-
 const titleCase = (str) => {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-      );
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+function Toogle() {
+  const connectScreen = document.getElementById("connectScreen");
+  const allData = document.getElementById("allData");
+  connectScreen.style.display = "none";
+  allData.style.display = "grid";
+}
+
+if (localStorage.getItem("key") !== null) {
+  Toogle();
+  getWeather("Paris");
 }
