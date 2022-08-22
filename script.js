762efee9bc18af616b073086c4cd70873444ac57
Amine <am1ne.maalouf7@gmail.com>
2022-08-22 17:03:09 +0300

    Good usesable version of the website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const key = 'be288bff8bd85a54d936b1e447023513';
+const key = '034fa528ccf9f7ebe825bbbff6f5e98f';
 
 const getWeather = (city) => {
     fetch(
@@ -16,12 +16,20 @@ const getWeather = (city) => {
 
 const display = (data) => {
     const { name } = data;
-    //const { icon, description } = data.weather[0];
+    const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
-    //const { speed } = data.wind;
+    const { speed } = data.wind;
     document.querySelector(".cityname").innerText = 'Weather in ' + name;
     document.querySelector(".temperature").innerText = temp + '°C';
-    document.querySelector(".humidity").innerText = humidity + '%';
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + '%';
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h"
+    document.querySelector(".description").innerText = titleCase(description);
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+
+    document.querySelector(".weatherdata").classList.remove("loading");
+
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
 }
 
 const search = () => {
@@ -39,3 +47,12 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
   });
 
 getWeather("Paris");
+
+const titleCase = (str) => {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      );
+}
